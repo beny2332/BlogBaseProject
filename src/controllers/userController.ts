8dd3c140d90eec95c2f6ecbefba8518c9b1d0a0b
BinyaminYacovson <bj57577@gmail.com>
2024-10-10 12:39:00 +0300

    create user get users and get user (by name) done and tested

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,37 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import User, { IUser } from "../models/userModel";
 
-export const createUser = async (req: Request, res: Response) => {};
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const newUser = await User.create(req.body)
+        res.status(201).json(newUser)
+    } catch (error) {
+        next(error)
+    }
+};
 
-export const getUsers = async (req: Request, res: Response) => {};
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch (error) {
+        next(error)
+    }
+};
 
-export const getUser = async (req: Request, res: Response) => {};
+export const getUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const username = req.params.username;
+        const user = await User.findOne({ username: username })
+        if (!user) {
+            const error = new Error("User not found")
+            error.name = "NotFoundError"
+            throw error
+        }
+        res.status(200).json(user)        
+    } catch (error) {
+        next(error)
+    }
+};
 
-// Optionally, add DELETE and EDIT functions
+
